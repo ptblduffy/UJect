3339b150900d5841bbde874b28cd664f81adf3ac
Eric McDuffee <eric@recroom.com>
2021-02-28 13:59:31 -0800

    Splitting resolvers into multiple classes

---
+++ Runtime/DIContainer/Resolvers/ResolverBase.cs
@@ -0,0 +1,13 @@
+﻿namespace UJect.Resolvers
+{
+    public interface IResolver
+    {
+        object Resolve();
+    }
+    
+    public abstract class ResolverBase<TImpl> : IResolver<TImpl>
+    {
+        public abstract TImpl ResolveTypedInstance();
+        object IResolver.Resolve() => ResolveTypedInstance();
+    }
+}
