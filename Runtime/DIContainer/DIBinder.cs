d683b7b9b1efa0bf859709a839b218378cbbab7f
Eric Bennett McDuffee <ericmcduffee246@gmail.com>
2021-02-28 14:05:46 -0800

    More code cleanup, and adding documentation

--- Runtime/DIContainer/DIBinder.cs
+++ Runtime/DIContainer/DIBinder.cs
@@ -7,12 +7,52 @@ namespace UJect
 {
     public interface IDiBinder<in TInterface>
     {
+        /// <summary>
+        /// Bind the type resource with the custom ID provided. This allows disambiguating multiple resources of the same type
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         IDiBinder<TInterface> WithId(string id);
+        
+        /// <summary>
+        /// Bind the given type to a provided concrete implementation instance of that type.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <typeparam name="TImpl"></typeparam>
+        /// <returns></returns>
         DiContainer ToInstance<TImpl>(TImpl instance) where TImpl : TInterface;
+        
+        /// <summary>
+        /// Bind the given type to a new instance of the provided concrete implementation of that type.
+        /// </summary>
+        /// <typeparam name="TImpl"></typeparam>
+        /// <returns></returns>
         DiContainer ToNewInstance<TImpl>() where TImpl : TInterface;
+        
+        /// <summary>
+        /// Bind the given type to a function that will provide a concrete instance of that type
+        /// </summary>
+        /// <param name="factoryMethod"></param>
+        /// <typeparam name="TImpl"></typeparam>
+        /// <returns></returns>
         DiContainer ToFactoryMethod<TImpl>(Func<TImpl> factoryMethod) where TImpl : TInterface;
+        
+        /// <summary>
+        /// Bind the given type to a factory implementation that will provide a concrete instance of that type.
+        /// Factories can be injected into before resolution, making them useful when you want to use a bunch of injected resources to
+        /// create the instance.
+        /// </summary>
+        /// <param name="factoryImpl"></param>
+        /// <typeparam name="TImpl"></typeparam>
+        /// <returns></returns>
         DiContainer ToFactory<TImpl>(IInstanceFactory<TImpl> factoryImpl) where TImpl : TInterface;
 
+        /// <summary>
+        /// Bind the given type to a Unity resource at the provided path. That resource will be loaded via Resource.Load<TImpl>(resourcePath);
+        /// </summary>
+        /// <param name="resourcePath"></param>
+        /// <typeparam name="TImpl"></typeparam>
+        /// <returns></returns>
         DiContainer ToResource<TImpl>(string resourcePath) where TImpl : Object, TInterface;
     }
 
--- Runtime/DIContainer/Resolvers/IResolver.cs
+++ Runtime/DIContainer/Resolvers/IResolver.cs
@@ -1,6 +1,19 @@
 ﻿namespace UJect.Resolvers
 {
-    public interface IResolver<TImpl> : IResolver
+    /// <summary>
+    /// IResolvers must be able to resolve a single instance of a dependency.
+    /// Resolve will be called once when the instance is first resolved. That instance will be cached internally
+    /// </summary>
+    public interface IResolver
+    {
+        object Resolve();
+    }
+    
+    /// <summary>
+    /// IResolvers must be able to resolve a single instance of a dependency.
+    /// Resolve will be called once when the instance is first resolved. That instance will be cached internally
+    /// </summary>
+    public interface IResolver<out TImpl> : IResolver
     {
         TImpl ResolveTypedInstance();
     }
