dab2a17c49ec66898c27c44089313886a5871b5f
Eric Bennett McDuffee <ericmcduffee246@gmail.com>
2022-11-29 08:57:51 -0800

    Adding multi-bind unity binders

--- UnityExtensions/ResourceBindings/ResourceInstanceResolver.cs
+++ UnityExtensions/ResourceBindings/ResourceInstanceResolver.cs
@@ -5,22 +5,6 @@ using UnityEngine;
 
 namespace UJect.UnityExtensions
 {
-    public static class UnityBinderExtensions
-    {
-        public static UnityDiBinder<TInterface> UnityBind<TInterface>(this DiContainer diContainer) => new UnityDiBinder<TInterface>(diContainer);
-    }
-
-    public class UnityDiBinder<TInterface> : DiBinder<TInterface>
-    {
-        public UnityDiBinder(DiContainer dependencies) : base(dependencies) { }
-        
-        public DiContainer ToResource<TImpl>(string resourcePath) where TImpl: Object, TInterface
-        {
-            var resolver = new ResourceInstanceResolver<TImpl>(resourcePath);
-            return ToCustomResolver<TImpl>(resolver);
-        }
-    }
-
     internal class ResourceInstanceResolver<TImpl> : ResolverBase<TImpl> where TImpl : Object
     {
         private readonly string resourcePath;
